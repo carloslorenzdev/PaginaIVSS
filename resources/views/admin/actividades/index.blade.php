f52b34fa70b99ffb973d90dfcaa33a2ed90f9704
CarlosLorenz20 <la2967163@gmail.com>
2026-07-07 12:30:15 -0400

    Corregir símbolo de interrogación (BOM) al inicio de archivos Blade y Seeders

--- resources/views/admin/actividades/index.blade.php
+++ resources/views/admin/actividades/index.blade.php
@@ -1,4 +1,4 @@
-?@extends('layouts/app')
+@extends('layouts/app')
 
 @section('titulo', 'Actividades Anuales')
 
--- resources/views/admin/chatbot/preguntas_sin_respuesta/index.blade.php
+++ resources/views/admin/chatbot/preguntas_sin_respuesta/index.blade.php
@@ -1,4 +1,4 @@
-?@extends('layouts/app')
+@extends('layouts/app')
 
 @section('titulo', 'Preguntas sin Respuesta')
 
